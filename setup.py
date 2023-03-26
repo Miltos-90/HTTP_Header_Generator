bb4f6ae8ef2868ccd5c25215efbe6ee4adccf9e3
Miltos-90 <35740991+Miltos-90@users.noreply.github.com>
2023-03-26 08:03:18 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 
 with open('./README.md', 'r') as f: README = f.read()
 
 setup(
     name="random-header-generator", 
-    version="1.1",
+    version="1.2",
     author="miltos_90",
     description='Generator of random, realistic http headers.',
     long_description=README,
     long_description_content_type="text/markdown",
     license="GNU General Public License v3.0",
-    packages=find_packages(),
     python_requires='>=3.10',
+    include_package_data = True,
+    packages=find_namespace_packages(),
     install_requires=["bs4", "requests"],
     keywords=['python', 'headers', 'http'],
     classifiers=[
